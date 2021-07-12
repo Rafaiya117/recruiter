366c22b61a4f29fc4bfe75a73cc847778a280ea1
Rafaiya117 <rushdha117@gmail.com>
2021-07-12 15:45:49 +0600

    Initial commit

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('master');
+});
+Route::get('/contact',function() {
+    return view('backend.layouts.contact');
+
+
 });
